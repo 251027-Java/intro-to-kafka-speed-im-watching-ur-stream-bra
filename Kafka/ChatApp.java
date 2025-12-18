9ede766e761c1691ec66495b08e3196f11eeb506
“Osi” <oobasuyi@uncc.edu>
2025-12-18 14:10:48 -0600

    intro to kafka with docker compose

--- Kafka/ChatApp.java
+++ Kafka/ChatApp.java
@@ -1,5 +1,11 @@
 package com.revature.lab;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
 public class ChatApp {
 
     public static void main(String[] args) {
